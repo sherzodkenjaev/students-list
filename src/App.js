3b58c47c6a345e718aa6adaa7e06320e5ac83393
sherzodkenjaev <kenjaevsherzod@gmail.com>
2021-08-17 14:30:36 +0500

    change in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Route, Switch } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import './App.css';
-import AddContact from './components/AddContact';
-import EditContact from './components/EditContact';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import { Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import AddContact from "./components/AddContact";
+import EditContact from "./components/EditContact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -12,14 +12,14 @@ function App() {
       <ToastContainer />
       <Navbar />
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
         <Route path="/add">
           <AddContact />
         </Route>
         <Route path="/edit/:id">
           <EditContact />
+        </Route>
+        <Route path="/">
+          <Home />
         </Route>
       </Switch>
     </div>
